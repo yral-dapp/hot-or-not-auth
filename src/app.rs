d7d4a03245a4c4b7ae5b758d63707b2a4045c814
Rohit Sarpotdar <rohit@gobazzinga.io>
2024-01-22 00:09:09 +0530

    Added build dependencies required for workflow

--- src/app.rs
+++ src/app.rs
@@ -45,13 +45,13 @@ fn HomePage() -> impl IntoView {
                     <img src="/logo-full.svg" class="h-[43px]"/>
                     <h5 class="font-inter first-letter:capitalize font-bold text-black text-xl mt-5 mb-3 text-sm leading-6 text-black"></h5>
                     <div class="flex items-center mt-5 space-x-1 text-sm">
-                        <span>Sign in to continue to <a class="transition-opacity text-linkColor hover:opacity-50" href="https://hotornot.wtf" target="_blank" rel="noreferrer">hotornot.wtf</a></span>
+                        <span>Sign in to continue to <a class="transition-opacity text-linkColor hover:opacity-50" href="https://hotornot.wtf" target="_blank" rel="noreferrer">"hotornot.wtf"</a></span>
                     </div>
                 </div>
                 <div class="mt-7">
                     <form class="space-y-[14px]">
                         <div class="rounded-md SENSITIVE_CONTENT_NO_SESSION_RECORDING">
-                            <label class="text-xs text-black leading-4 text-xs">Email</label>
+                            <label class="text-xs text-black leading-4 text-xs">"Email"</label>
                             <div class="flex relative">
                                 <input type="email" class="flex-1 block w-full py-[7px] placeholder:text-secondary placeholder:text-sm disabled:bg-gray-200 disabled:text-secondary disabled:drop-shadow-none shadow-none border-1 border-gray-400 disabled:border-gray-200 rounded-md focus:ring-[3px] active:ring-blue-200 focus:ring-blue-200 active:border-blue-base active:bg-blue-50 h-12" name="email" autocomplete="off webauthn"/>
                                 <span class="absolute right-0 h-full -translate-y-1/2 top-1/2 items-center flex"></span>
@@ -59,13 +59,13 @@ fn HomePage() -> impl IntoView {
                         </div>
                         <button id="email-sign-button" class="transition duration-75 text-center text-sm first-letter:capitalize hover:no-underline font-bold border rounded-md outline-none p-[15px] leading-4 cursor-pointer disabled:cursor-not-allowed focus:ring-2 focus:ring-offset-[1px] focus:ring-black p-[15px] text-white border-transparent bg-primaryButtonColor hover:shadow-md hover:shadow-primaryButtonColor/40 active:bg-primaryButtonColor active:border-primaryButtonColor focus:border-primaryButtonColor focus:bg-primaryButtonColor disabled:bg-primaryButtonColor/20 disabled:shadow-none w-full block h-12 !p-0">
                             <div class="flex items-center justify-center space-x-2">
-                                <div class="text-center">Continue with email</div>
+                                <div class="text-center">"Continue with email"</div>
                             </div>
                         </button>
                     </form>
                     <div class="flex items-center justify-between w-full text-sm text-secondary h-9 my-5">
                         <div class="flex w-full h-[1px] bg-gray-300"></div>
-                        <div class="px-2">OR</div>
+                        <div class="px-2">"OR"</div>
                         <div class="flex w-full h-[1px] bg-gray-300"></div>
                     </div>
                     <div class="hidden">
@@ -88,7 +88,7 @@ fn HomePage() -> impl IntoView {
                                         <path fill="#EA4335" d="M12.2199 5.82746C13.7269 5.82746 15.0722 6.34683 16.1365 7.36002L19.0483 4.44816C17.2859 2.7964 14.9785 1.7832 12.2199 1.7832C8.22676 1.7832 4.7785 4.08205 3.1012 7.41962L6.48986 10.0505C7.29871 7.62396 9.55498 5.82746 12.2199 5.82746Z"></path>
                                     </svg>
                                 </div>
-                                <div class="text-center">Continue with Google</div>
+                                <div class="text-center">"Continue with Google"</div>
                             </div>
                         </button>
                     </div>
